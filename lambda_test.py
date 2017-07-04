a410be968f8bff40ab224e56073abb9f38fd894e
lucifercr07 <prashant1996cr07@gmail.com>
2017-07-04 12:50:45 +0530

    Removed unwanted files

--- lambda_test.py
+++ lambda_test.py
@@ -11,3 +11,13 @@ while(1):
     print("RPM: ",get_rpm())
     print("Vibration: ",get_vibration())
     time.sleep(1)
+
+rpm = [45,20,30,50,52,35,55,65,60,66,60]
+
+rpm_limit=45
+
+def check():
+    max_consec = reduce(lambda acc, r: acc + 1 if r > rpm_limit else 0, rpm, 0)
+    return 1 if max_consec >= 5 else 0
+    
+print(check())
